6f2da2ad3fcc1b44b4c28280ed0f19512135eac3
vphpersson <vph.persson@gmail.com>
2015-09-23 00:12:28 +0200

    Rudimentär request-hantering.

--- webserver/src/server.c
+++ webserver/src/server.c
@@ -4,26 +4,114 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+#include <fcntl.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <limits.h>
 
 #define MYPORT "3490"  // the port users will be connecting to
 #define BACKLOG 10     // how many pending connections queue will hold
+#define MAXBUFLEN 1024
 
-void error(const char *msg)
+
+int send_all(int sockfd, void *msg, size_t len, int flag)
 {
-        fprintf(stderr, msg);
-        exit(1);
+        char *ptr = (char*) msg;
+        while (len > 0) {
+                int rec = send(sockfd, ptr, len, flag);
+                printf("Sent: %d.\n", rec);
+                if (rec < 1)
+                        return 0;
+                ptr += rec;
+                len -= rec;
+        }
+
+        return -1;
+}
+
+void get_request(int sockfd, char *token)
+{
+        int in_fd;
+        char buf[1024];
+        char *filename;
+        struct stat stat_buf;      /* argument to fstat */
+
+        if ((token = strtok(NULL, " ")) == NULL)
+                perror("token 2");
+        
+        if ((filename = realpath(token, buf)))
+                printf("filename: %s\n", filename);
+        else {
+                perror("realpath");
+                // code 4xx
+        }
+
+        if ((in_fd = open(filename, O_RDONLY)) == -1)
+                perror("open");
+
+        fstat(in_fd, &stat_buf);
+
+        char *bestmsg = "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html; charset=UTF-8\r\n\r\n";
+
+        off_t offset = 0;
+        int sender = send(sockfd, bestmsg, strlen(bestmsg), 0);
+        int rc = sendfile(sockfd, in_fd, &offset, stat_buf.st_size);
+
+        close(in_fd);
+}
+
+void head_request(int sockfd, char *token)
+{
+        int a = 1;
+}
+
+void accept_request(int sockfd)
+{
+        //char buf[1024] = "GET ../thttpd.log HTTP/1.1";
+        char buf[1024];
+
+        if (recv(sockfd, buf, 1024-1, 0) < 1)
+               perror("recv");
+
+        const char *delim = " ";
+        char *token;
+
+        if ((token = strtok(buf, delim)) == NULL)
+                perror("arg");
+
+        if (strcmp(token, "GET") == 0) {
+                get_request(sockfd, token);
+        } else if (strcmp(token, "HEAD") == 0) {
+                head_request(sockfd, token);
+        } else
+                perror("not a method");
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
 int main(void)
 {
+
         struct sockaddr_storage their_addr;
         socklen_t addr_size;
         struct addrinfo hints, *res;
         int sockfd, new_fd, status, len, bytes_sent;
-        char *msg = "Viktor was here!";
+        char s[INET6_ADDRSTRLEN];
+
+        if (chroot("./www") != 0)
+                perror("chroot");
 
         memset(&hints, 0, sizeof hints);
         hints.ai_family = AF_UNSPEC;
@@ -35,25 +123,42 @@ int main(void)
                 return 1;
         }
 
-        if ((sockfd = 
-            socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1) {
-                error("Error opening socket!\n");
+        if ((sockfd = socket(res->ai_family, res->ai_socktype, 
+            res->ai_protocol)) == -1) {
+                perror("socket");
         }
 
-        if (bind(sockfd, res->ai_addr, res->ai_addrlen) != 0)
-                error("Bind failed!\n");
+        if (bind(sockfd, res->ai_addr, res->ai_addrlen) != 0) {
+                close(sockfd);
+                perror("bind");
+        }
 
         if (listen(sockfd, BACKLOG) != 0)
-                error("Listen failed!\n");
+                perror("listen");
 
-        addr_size = sizeof their_addr;
-        if ((new_fd = 
-            accept(sockfd, (struct sockaddr *)&their_addr, &addr_size)) == -1) {
-                error("Accept failed!\n");
+        while(1) {
+                addr_size = sizeof their_addr;    
+                if ((new_fd = accept(sockfd, (struct sockaddr *) &their_addr, 
+                    &addr_size)) == -1) {
+                        perror("accept");
+                        continue;
+                }
+
+                inet_ntop(their_addr.ss_family, 
+                    get_in_addr((struct sockaddr *) &their_addr), s, sizeof s);
+
+                printf("Server: got connection from %s\n", s);
+
+                if (!fork()) { // this is the child process
+                        close(sockfd); // child doesn't need the listener
+                        accept_request(new_fd);
+                        //if (send_all(new_fd, msg, strlen(msg), 0) == -1)
+                        //        perror("send");
+                        close(new_fd);
+                        exit(0);
+                }
+                close(new_fd);  // parent doesn't need this
         }
-
-        bytes_sent = send(new_fd, msg, strlen(msg), 0);
-        close(new_fd);
-
+        
         return 0;
 }
